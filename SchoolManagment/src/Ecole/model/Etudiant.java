983eddd82704f786dc4424f1ae91bf9df0014aeb
RachidTarchouli23 <rachidtarchouli444@gmail.com>
2025-03-24 01:47:44 +0000

    changement propriete etudiant ages

--- SchoolManagment/src/Ecole/model/Etudiant.java
+++ SchoolManagment/src/Ecole/model/Etudiant.java
@@ -4,20 +4,20 @@ public class Etudiant {
 
 
     private String nom;
-    private int age;
+    private int ages;
 
     public Etudiant(String nom, int age) {
         this.nom = nom;
-        this.age = age;
+        this.ages = age;
     }
 
     public String getNom() {
         return nom; }
     public int getAge() {
-        return age; }
+        return ages; }
 
     @Override
     public String toString() {
-        return "Etudiant{" + "nom='" + nom  + ", age=" + age + '}';
+        return "Etudiant{" + "nom='" + nom  + ", age=" + ages + '}';
     }
 }
